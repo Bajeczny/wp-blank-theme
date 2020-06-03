cba3e24787f92545d81884d1234c33cd9df02622
Adam <adam.kordel@gmail.com>
2020-06-03 14:27:26 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,34 @@
+<?php
 
+/*
+ * Footer
+ * */
+
+?>
+
+        </div>
+
+        <footer id="main-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-xl-9 col-md-12 col-12">
+                        <div class="component text-component">
+                            <div class="component-inner">
+                                <p>Klient 2020. Wszelkie prawa zastrzeżone.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-xl-3 col-md-12 col-12">
+                        <div class="component text-component">
+                            <div class="component-inner">
+                              <p>Wdrożenie: <a href="https://fastsite.pl/" target="_blank">FASTSITE</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
